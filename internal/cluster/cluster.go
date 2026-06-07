5d0c00a8e4342d3b834e1b78c306d353313019db
eswar-7116 <eswardudi06@gmail.com>
2026-06-07 18:25:08 +0530

    refactor(cluster): use defer for unlocking mutex

--- internal/cluster/cluster.go
+++ internal/cluster/cluster.go
@@ -23,37 +23,36 @@ func NewCluster(replicas int) *Cluster {
 
 func (c *Cluster) CreatePod() {
 	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.pods = append(c.pods, Pod{
 		Id: c.nextId,
 	})
 	fmt.Println("Created pod with id", c.nextId)
 	c.nextId++
-	c.mu.Unlock()
 }
 
 func (c *Cluster) TerminatePod() {
 	c.mu.Lock()
+	defer c.mu.Unlock()
 	if len(c.pods) == 0 {
-		c.mu.Unlock()
 		return
 	}
 
 	id := c.pods[len(c.pods)-1].Id
 	c.pods = c.pods[:len(c.pods)-1]
-	c.mu.Unlock()
 	fmt.Println("Terminated pod with id", id)
 }
 
 func (c *Cluster) SetReplicas(replicas int) {
 	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.deployment.Replicas = replicas
-	c.mu.Unlock()
 }
 
 func (c *Cluster) State() (pods, replicas int) {
 	c.mu.RLock()
+	defer c.mu.RUnlock()
 	pods = len(c.pods)
 	replicas = c.deployment.Replicas
-	c.mu.RUnlock()
 	return
 }
